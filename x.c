ba8c8e09b42078c9b943e35a1b12f5e701597f7e
deyannis <deyannis@csd.uoc.gr>
2025-07-13 04:17:12 +0200

    patch: st-colors-at-launch-0.8.4.diff

--- x.c
+++ x.c
@@ -2079,6 +2079,8 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
+    int i;
+    char *colval;
 	xw.l = xw.t = 0;
 	xw.isfixed = False;
 	xsetcursor(cursorshape);
@@ -2125,6 +2127,11 @@ main(int argc, char *argv[])
 		break;
 	case 'v':
 		die("%s " VERSION "\n", argv0);
+		break;
+    case 'C':
+        colval = strtok(EARGF(usage()), "@");
+        i = atoi(strtok(NULL, "@"));
+		colorname[i] = colval;
 		break;
 	default:
 		usage();
